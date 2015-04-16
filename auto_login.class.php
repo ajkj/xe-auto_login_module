418c8d916cbbd451759b3e6a3ab941c52c5e0732
ajkj <thename@requery.me>
2015-04-17 01:20:03 +0900

    version 1.0.3

--- auto_login.class.php
+++ auto_login.class.php
@@ -20,8 +20,8 @@ class auto_login extends ModuleObject {
 
         $this->module_self_info = new stdClass();
         $this->module_self_info->module_name = 'auto_login';
-        $this->module_self_info->module_version_code = 3;
-        $this->module_self_info->module_version_name = '1.0.2';
+        $this->module_self_info->module_version_code = 4;
+        $this->module_self_info->module_version_name = '1.0.3';
 
 
         $this->config_session = new stdClass();
